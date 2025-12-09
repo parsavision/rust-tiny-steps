265e94d9ca34ca6905bda8c7863d9cb4662b14dd
parsavision <224815858+parsavision@users.noreply.github.com>
2025-12-09 19:56:46 +0330

    update ex 169

---
+++ ex169_mutable_struct/target/debug/deps/ex169_mutable_struct-dd40dd5f319b8181.d
@@ -0,0 +1,5 @@
+/home/parsavisions/Desktop/github/rust-tiny-steps/ex169_mutable_struct/target/debug/deps/ex169_mutable_struct-dd40dd5f319b8181.d: src/main.rs
+
+/home/parsavisions/Desktop/github/rust-tiny-steps/ex169_mutable_struct/target/debug/deps/ex169_mutable_struct-dd40dd5f319b8181: src/main.rs
+
+src/main.rs:
---
+++ ex169_mutable_struct/target/debug/ex169_mutable_struct.d
@@ -0,0 +1,1 @@
+/home/parsavisions/Desktop/github/rust-tiny-steps/ex169_mutable_struct/target/debug/ex169_mutable_struct: /home/parsavisions/Desktop/github/rust-tiny-steps/ex169_mutable_struct/src/main.rs
