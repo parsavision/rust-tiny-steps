260520a10d4da1e8a6102f0c9f0b2aace9e52349
parsavision <224815858+parsavision@users.noreply.github.com>
2025-12-09 19:56:10 +0330

    update ex 169

--- ex169_mutable_struct/src/main.rs
+++ ex169_mutable_struct/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    struct Users {
+        name: String,
+        age: i32,
+    }
+
+    let mut user = Users {
+        name: String::from("John Doe"),
+        age: 30,
+    };
+    println!("Name: {}", user.name);
+    println!("Age: {}", user.age);
+    user.age = 31;
+    println!("Updated Age: {}", user.age);
 }
