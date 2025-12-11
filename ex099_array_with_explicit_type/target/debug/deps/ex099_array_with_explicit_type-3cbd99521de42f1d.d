37b3af57bc0687f53f97775eeb5c4bc3771dae10
parsavision <224815858+parsavision@users.noreply.github.com>
2025-12-12 00:35:06 +0330

    add ex 99

---
+++ ex099_array_with_explicit_type/target/debug/deps/ex099_array_with_explicit_type-3cbd99521de42f1d.d
@@ -0,0 +1,5 @@
+/home/parsavisions/Desktop/github/rust-tiny-steps/ex099_array_with_explicit_type/target/debug/deps/ex099_array_with_explicit_type-3cbd99521de42f1d.d: src/main.rs
+
+/home/parsavisions/Desktop/github/rust-tiny-steps/ex099_array_with_explicit_type/target/debug/deps/libex099_array_with_explicit_type-3cbd99521de42f1d.rmeta: src/main.rs
+
+src/main.rs:
---
+++ ex099_array_with_explicit_type/target/debug/deps/ex099_array_with_explicit_type-8b889dd47beced7f.d
@@ -0,0 +1,5 @@
+/home/parsavisions/Desktop/github/rust-tiny-steps/ex099_array_with_explicit_type/target/debug/deps/ex099_array_with_explicit_type-8b889dd47beced7f.d: src/main.rs
+
+/home/parsavisions/Desktop/github/rust-tiny-steps/ex099_array_with_explicit_type/target/debug/deps/libex099_array_with_explicit_type-8b889dd47beced7f.rmeta: src/main.rs
+
+src/main.rs:
